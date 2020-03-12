6e28dfa59d48883331234ea39ab03631b4c0e5da
Luca Vaccaro <luck87@gmail.com>
2020-03-12 06:36:18 +0100

    build libraries first

--- fetchbuild.sh
+++ fetchbuild.sh
@@ -3,18 +3,34 @@ set -e
 
 repo=$1
 commit=$2
-target_host=$3
-bits=$4
-build=$5
-rootdir=$6
+reponame=$3
+rename=$4
+configextra=$5
+target_host=$6
+bits=$7
 
-export PATH=/opt/toolchain/bin:${PATH}
-export AR=${target_host}-ar
-export AS=${target_host}-as
-export CC=${target_host}26-clang
-export CXX=${target_host}26-clang++
-export LD=${target_host}-ld
-export STRIP=${target_host}-strip
+unpackdep() {
+    archive=$(basename $1)
+    curl -sL -o ${archive} $1
+    echo "$2 ${archive}" | sha256sum --check
+    tar xf ${archive}
+    rm ${archive}
+}
+
+# build deps
+LNBUILDROOT=$PWD/build_root
+mkdir $LNBUILDROOT
+TORBUILDROOT=$PWD/tor_build_root
+mkdir $TORBUILDROOT
+
+export ANDROID_NDK_HOME=/opt/android-ndk-r20b
+export PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:${PATH}
+export AR=${target_host/v7a}-ar
+export AS=${target_host}24-clang
+export CC=${target_host}24-clang
+export CXX=${target_host}24-clang++
+export LD=${target_host/v7a}-ld
+export STRIP=${target_host/v7a}-strip
 export LDFLAGS="-pie"
 export MAKE_HOST=${target_host}
 export HOST=${target_host}
@@ -32,9 +48,8 @@ if [ -f /proc/cpuinfo ]; then
     num_jobs=$(grep ^processor /proc/cpuinfo | wc -l)
 fi
 
-# sqlite
-wget https://www.sqlite.org/2018/sqlite-autoconf-3260000.tar.gz
-tar xzvf sqlite-autoconf-3260000.tar.gz
+# build sqlite
+unpackdep https://www.sqlite.org/2018/sqlite-autoconf-3260000.tar.gz 5daa6a3fb7d1e8c767cd59c4ded8da6e4b00c61d3b466d0685e35c4dd6d7bf5d
 cd sqlite-autoconf-3260000
 ./configure --enable-static --disable-readline --disable-threadsafe --host=${target_host} CC=$CC --prefix=${QEMU_LD_PREFIX}
 make -j $num_jobs
@@ -43,9 +58,8 @@ cd ..
 rm -rf sqlite-autoconf-3260000
 rm -rf sqlite-autoconf-3260000.tar.gz
 
-# gmp
-wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
-tar xjvf gmp-6.1.2.tar.bz2
+# build gmp
+unpackdep https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
 cd gmp-6.1.2
 ./configure --enable-static --disable-assembly --host=${target_host} CC=$CC --prefix=${QEMU_LD_PREFIX}
 make -j $num_jobs
@@ -53,6 +67,40 @@ make install
 cd ..
 rm -rf gmp-6.1.2
 rm -rf gmp-6.1.2.tar.bz2
+
+# build libevent
+unpackdep https://github.com/libevent/libevent/archive/release-2.1.11-stable.tar.gz 229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e
+cd libevent-release-2.1.11-stable
+./autogen.sh
+./configure --prefix=$TORBUILDROOT/libevent --enable-static --disable-samples \
+            --disable-openssl --disable-shared --disable-libevent-regress --disable-debug-mode \
+            --disable-dependency-tracking --host $target_host
+make -o configure install -j${num_jobs}
+cd ..
+
+# build zlib
+unpackdep https://github.com/madler/zlib/archive/v1.2.11.tar.gz 629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff
+cd zlib-1.2.11
+
+./configure --static --prefix=$TORBUILDROOT/zlib
+make -o configure install -j${num_jobs}
+cd ..
+
+# build openssl
+unpackdep https://github.com/openssl/openssl/archive/OpenSSL_1_1_1d.tar.gz 23011a5cc78e53d0dc98dfa608c51e72bcd350aa57df74c5d5574ba4ffb62e74
+
+cd openssl-OpenSSL_1_1_1d
+SSLOPT="no-gost no-shared no-dso no-ssl3 no-idea no-hw no-dtls no-dtls1 \
+        no-weak-ssl-ciphers no-comp -fvisibility=hidden no-err no-psk no-srp"
+
+if [ "$bits" = "64" ]; then
+    SSLOPT="$SSLOPT enable-ec_nistp_64_gcc_128"
+fi
+./Configure android-$NDKARCH --prefix=$TORBUILDROOT/openssl $SSLOPT
+make depend
+make -j${num_jobs} 2> /dev/null
+make install_sw
+cd ..
 
 # download lightning
 git clone $repo lightning
@@ -90,7 +138,6 @@ make -j $num_jobs PIE=1 DEVELOPER=0
 deactivate
 cd ..
 
-
 export CFLAGS="-flto"
 export LDFLAGS="$CFLAGS -pie -static-libstdc++ -fuse-ld=lld"
 # build core
@@ -107,33 +154,23 @@ cp ${rootdir}/config.vars .
 cp ${rootdir}/config.h ./ccan
 cp ${rootdir}/gen_header_versions.h .
 
-# update arch based on toolchain
-sed -i -e 's/PREFIX=\/opt\/toolchain\/aarch64-linux-android-clang\/sysroot/PREFIX=\/opt\/toolchain\/sysroot/g' ./config.vars
-sed -i -e 's/CC=aarch64-linux-android-clang/CC='${CC}'/g' ./config.vars
-sed -i -e 's/#define CCAN_COMPILER "aarch64-linux-android-clang"/#define CCAN_COMPILER "'${CC}'"/g' ./ccan/config.h
+# build tor
+unpackdep https://github.com/torproject/tor/archive/tor-0.4.2.5.tar.gz 94ad248f4d852a8f38bd8902a12b9f41897c76e389fcd5b8a7d272aa265fd6c9
+cd tor-tor-0.4.2.5
+./autogen.sh
+TOROPT="--disable-system-torrc --disable-asciidoc --enable-static-tor --enable-static-openssl \
+        --with-zlib-dir=$TORBUILDROOT/zlib --disable-systemd --disable-zstd \
+        --enable-static-libevent --enable-static-zlib --disable-system-torrc \
+        --with-openssl-dir=$TORBUILDROOT/openssl --disable-unittests \
+        --with-libevent-dir=$TORBUILDROOT/libevent --disable-lzma \
+        --disable-tool-name-check --disable-rust \
+        --disable-largefile ac_cv_c_bigendian=no \
+        --disable-module-dirauth"
 
-# patch makefile
-git apply ${rootdir}/Makefile.patch
-
-# patch abstracted namespace for socket
-git apply ${rootdir}/jsonrpc.patch
-
-# patch endian.h if just defined
-git apply ${rootdir}/endian.patch
-
-# build external libraries and source before ccan tools
-make PIE=1 DEVELOPER=0 || echo "continue"
-
-# build ccan tools for the host machine
-#sed -i -e 's/"ccan_compat.h"/"..\/ccan_compat.h"/g' ccan/config.h
-make clean -C ccan/ccan/cdump/tools
-make LDFLAGS="" CC="${CONFIGURATOR_CC}" LDLIBS="-L/usr/local/lib" -C ccan/ccan/cdump/tools
-
-# complete the build process
-make PIE=1 DEVELOPER=0
-
-#exit from lightning build system
-deactivate
+./configure $TOROPT --prefix=$TORBUILDROOT/tor --host=$target_host --disable-android
+make -o configure install -j${num_jobs}
+$STRIP $TORBUILDROOT/tor/bin/tor
+mv $TORBUILDROOT/tor/bin/tor ../${reponame}/depends/${target_host/v7a/}/bin
 cd ..
 
 # packaging
